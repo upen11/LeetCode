f2bdba1d75c494871c7e8fd13d9f42cf773ebc99
Upendra Singh Panwar <50030097+upen11@users.noreply.github.com>
2022-12-13 19:47:26 +0530

    Dec 12

---
+++ Daily Challenge/12 - December/70. Climbing Stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        int a = 0;
+        int b = 1;
+        
+        int c = 1;
+        for(int i=1; i<=n; i++) {
+            c = a + b;
+            
+            a = b;
+            b = c;
+        }
+        
+        return c;
+    }
+}
